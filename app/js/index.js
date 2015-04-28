38e9fbccfb8fe03342afee6ec7ebb271ad94b08d
Max <mhmad.hamster@gmail.com>
2015-04-28 18:54:56 +0300

    few pages

--- app/js/index.js
+++ app/js/index.js
@@ -2,6 +2,25 @@ $(document).ready(function() {
 
     $('input, select').styler();
 
-    $("#range_1").ionRangeSlider();
+    $('.slider').noUiSlider({
+        start: [18500],
+        step: 500,
+        range: {
+            'min': [0],
+            'max': [50000]
+        },
+        format: wNumb({
+            decimals: 0,
+            thousand: ' ',
+            postfix: ' <small>руб</small>'
+        })
+    })
 
+    // $("#slider").Link('lower').to('-inline-<div class="tooltip"></div>');
+
+    $('.slider').Link('lower').to('-inline-<div class="tooltip"></div>', function(value) {
+        $(this).html(
+                value 
+            );
+    });
 })
